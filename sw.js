82a877d482abe55d4a577e81342dba11684f15c7
nadatef <72985252+nadatef@users.noreply.github.com>
2025-06-11 11:24:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'trybook-v1';
 const urlsToCache = [
-  '/trybook/',
-  '/trybook/index.htm',
-  '/trybook/manifest.json',
-  '/trybook/sw.js',
-  '/trybook/icon-192x192.png',
-  '/trybook/icon-512x512.png'
+  'index.html',      // No prefix!
+  'manifest.json',
+  'sw.js',
+  '6.jpg',
+  '7.jpg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
